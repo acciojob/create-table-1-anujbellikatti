7d13a5675d1590fd260bfe1c3fe81381a616ac85
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-25 14:59:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	// let	table = document.getElementById("sampleTable")
+	// let tr = docuent.createElement("tr");
+	// let td = document.createElement("td");
+	// let t1 = document.createElement("td");
+	// td.innerText = "hello"	
+	// t1.innerText = "hello"	
+	//  tr.appendChild(td);
+ //    tr.appendChild(t1);
+ //    table.appendChild(tr);
+	var table = document.getElementById('sampleTable');
+	var newRow = table.insertRow(0);
+
+	var cell1 = newRow.insertCell(0);
+	var cell2 = newRow.insertCell(1);
+
+	cell1.innerHTML = 'New Cell1';
+	cell2.innerHTML = 'New Cell2';
 }
